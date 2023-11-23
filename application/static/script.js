01bb435d769b7ce9a1195d1418f6a9bfa1172c48
ezrazka <ezra.a.einnos@gmail.com>
2023-11-23 16:43:23 +0700

    Added CSS for home page, added like function.

---
+++ application/static/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+document.querySelectorAll("#like-btn").forEach(btn => {
+    btn.addEventListener("click", function() {
+        const btnIcon = btn.querySelector('i');
+        const likeCounter = btn.closest('.post').querySelector('.like-counter');
+
+        btnIcon.classList.toggle('fa-heart');
+        btnIcon.classList.toggle('fa-heart-o');
+        btnIcon.classList.toggle('liked');
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/like', true);
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                // console.log(response.status);
+
+                let currentLikes = parseInt(likeCounter.textContent.match(/\d+/)[0]);
+
+                if (btnIcon.classList.contains('liked')) {
+                    currentLikes++;
+                } else {
+                    currentLikes--;
+                }
+
+                if (currentLikes === 1) {
+                    likeCounter.textContent = '1 like';
+                } else {
+                    likeCounter.textContent = `${currentLikes} likes`
+                }
+            }
+        }
+        const data = JSON.stringify({ 'postId': this.dataset.postId });
+        xhr.send(data);
+    });
+});
