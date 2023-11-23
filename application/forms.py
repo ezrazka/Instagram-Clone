b441463e0619c7b4e3044dc2f9046c205caf4297
ezrazka <ezra.a.einnos@gmail.com>
2023-11-23 08:29:30 +0700

    Added CSS for the login and signup pages.

--- application/forms.py
+++ application/forms.py
@@ -6,16 +6,16 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 from application.utils import exists_email, not_exists_email, exists_username, not_exists_username
 
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired(), not_exists_username])
-    password = PasswordField("password", validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired(), not_exists_username], render_kw={"placeholder": "Enter your username"})
+    password = PasswordField("password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
     submit = SubmitField("login")
 
 class SignUpForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
-    fullname = StringField("full name", validators=[Length(min=4, max=16)])
-    email = EmailField("email", validators=[DataRequired(), Email(), exists_email])
-    password = PasswordField("password", validators=[DataRequired(), Length(min=8)])
-    confirm_password = PasswordField("confirm password", validators=[DataRequired(), EqualTo("password", message="Password must match")])
+    username = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username], render_kw={"placeholder": "Enter your username"})
+    fullname = StringField("full name", validators=[Length(min=4, max=16)], render_kw={"placeholder": "Enter your full name"})
+    email = EmailField("email", validators=[DataRequired(), Email(), exists_email], render_kw={"placeholder": "Enter a valid email address"})
+    password = PasswordField("password", validators=[DataRequired(), Length(min=8)], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField("confirm password", validators=[DataRequired(), EqualTo("password", message="Password must match")], render_kw={"placeholder": "Confirm password"})
     submit = SubmitField("sign up")
 
 class EditProfileForm(SignUpForm):
